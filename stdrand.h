5b10c3386c11567d3dde3ee2879709f90ae585a5
DreamKillDemon <129873819+DreamKillDemon@users.noreply.github.com>
2023-11-11 19:13:32 +0700

    Update stdrand.h

--- stdrand.h
+++ stdrand.h
@@ -1,17 +1,18 @@
-// thư viện này dùng để thực hiện random số ngẫu nhiên mỗi khi gọi
 #pragma once
+
 #include<stdlib.h>
 #include<math.h>
 #include<time.h>
 
 // Khởi tạo giá trị seed cho hàm rand() bằng thời gian hiện tại
-void _Random() 
+
+inline void _Random()
 {
     srand((unsigned int)time(NULL));
 }
 
 // trả về giá trị từ 0 -> a
-int _rand(int a)
+inline int _rand(int a)
 {
     return rand() % a;
-}  
+}
